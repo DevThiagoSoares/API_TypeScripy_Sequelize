d4877c986c431718a7e55dfa58b91d2642820234
ThiagoCardosoSoares <100891121+ThiagoCardosoSoares@users.noreply.github.com>
2022-05-31 15:26:41 -0400

    Controllers criados

---
+++ controller/ClientsController.ts
@@ -0,0 +1,87 @@
+import { Request, Response } from "express";
+import { Clients } from "../src/models/clients";
+
+export const clientsController = {
+    index: async (req: Request, res: Response) => {
+        try {
+            const clients = await Clients.findAll();
+            return res.json(clients)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message });
+            }
+        }
+    },
+
+    // POST /clients
+    save: async (req: Request, res: Response) => {
+        const { name, phone } = req.body;
+
+        try {
+            const clients = await Clients.create({
+                name,
+                phone
+            });
+
+            return res.status(201).json(clients)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message });
+            }
+        }
+    },
+
+    // GET /clients/:id
+    show: async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        try {
+            const clients = await Clients.findByPk(id);
+            return res.json(clients)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message });
+            }
+        }
+    },
+
+    // PUT /candidates/:id
+    update: async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const { name, phone } = req.body;
+
+        try {
+            const clients = await Clients.findByPk(id);
+
+            if (clients === null) {
+                return res.status(404).json({ message: 'cliente não encontrado' });
+            }
+
+            clients.name = name;
+            clients.phone = phone;
+
+            await clients.save();
+            return res.status(201).json(clients);
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message });
+            }
+        }
+    },
+
+
+    //DELETE /candidates/:id
+    delete: async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        try {
+            await Clients.destroy({ where: { id } });
+
+            return res.status(204).send;
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message });
+            }
+        }
+    }
+}
---
+++ controller/RestaurantsController.ts
@@ -0,0 +1,93 @@
+import { Request, Response } from 'express';
+import { Restaurants } from '../src/models/restaurants';
+
+export const RestaurantsController = {
+
+    // GET /candidates
+    index: async (req: Request, res: Response) => {
+        try {
+            const restaurants = await Restaurants.findAll();
+            return res.json(restaurants);
+
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message });
+            }
+        }
+    },
+
+    // POST /candidates
+    save: async (req: Request, res: Response) => {
+        const { name, phone, description, address } = req.body;
+
+        try {
+            const restaurants = await Restaurants.create({
+                name,
+                phone,
+                description,
+                address
+            });
+
+            return res.status(201).json(restaurants)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message });
+            }
+        }
+    },
+
+    // GET /candidates/:id
+    show: async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        try {
+            const restaurants = await Restaurants.findByPk(id);
+            return res.json(restaurants)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message });
+            }
+        }
+    },
+
+    // PUT /candidates/:id
+    update: async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const { name, phone, description, address } = req.body;
+
+        try {
+            const restaurants = await Restaurants.findByPk(id);
+
+            if (restaurants === null) {
+                return res.status(404).json({ message: 'restaurante não encontrado' });
+            }
+
+            restaurants.name = name;
+            restaurants.phone = phone;
+            restaurants.description = description;
+            restaurants.address = address;
+
+            await restaurants.save();
+            return res.status(201).json(restaurants);
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message });
+            }
+        }
+    },
+
+    //DELETE /candidates/:id
+    delete: async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        try {
+            await Restaurants.destroy({ where: { id } });
+
+            return res.status(204).send;
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message });
+            }
+        }
+    }
+}
