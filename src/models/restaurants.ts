a55555249bfed9d12b3556969864b0690ab77fee
ThiagoCardosoSoares <100891121+ThiagoCardosoSoares@users.noreply.github.com>
2022-05-31 12:10:41 -0400

    models finalizado

--- src/models/restaurants.ts
+++ src/models/restaurants.ts
@@ -8,3 +8,28 @@ interface RestaurantsInstance extends Model {
     description: string;
     address: string;
 };
+
+export const Restaurants = sequelize.define<RestaurantsInstance>(
+    'restaurants',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        phone: {
+            type: DataTypes.STRING
+        },
+        description: {
+            type: DataTypes.TEXT
+        },
+        address: {
+            type: DataTypes.TEXT
+        }
+    }
+);
