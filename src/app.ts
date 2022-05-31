5711159c7f94789bd601fee247911e2ca89a8ecd
ThiagoCardosoSoares <100891121+ThiagoCardosoSoares@users.noreply.github.com>
2022-05-31 12:15:18 -0400

    rota restaurants criada

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import { Restaurants } from './models/restaurants';
 
 const app = express();
 const router = express.Router();
 
 router.get('/', (req, res) => {res.json({hello: 'hello, world!'})});
+router.get('/restaurants',async (req, res) => {
+    const restaurants = await Restaurants.findAll();
+    return res.json(restaurants);
+})
 
 app.use(router);
 
